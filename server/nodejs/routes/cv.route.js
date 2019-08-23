d7db12cae0672f76bc0278a900dc8d333da1ce77
Jeff <jeffscottlord@gmail.com>
2019-08-23 16:54:58 -0400

    working on file upload

--- server/nodejs/routes/cv.route.js
+++ server/nodejs/routes/cv.route.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const path = require('path');
+// const formidable = require('formidable');
+const multer = require('multer');
+const fs = require('fs');
 const cv = require('../modules/cv_parse');
+
+const upload = multer({ dest: 'uploads/' });
 
 const router = express.Router();
 
@@ -22,6 +27,22 @@ router.get('/analyze', (req, res, next) => {
       });
     });
   });
+});
+
+router.post('/upload', upload.single('files'), (req, res, next) => {
+  console.log('file', req.file);
+
+  console.log('body', req.body);
+
+  fs.unlink(req.file, (err) => {
+    res.send(req.body);
+  });
+});
+router.post('/uploadMany', upload.array('files'), (req, res, next) => {
+  console.log('files', req.files);
+
+  console.log('body', req.body);
+  res.send('done');
 });
 
 module.exports = router;
