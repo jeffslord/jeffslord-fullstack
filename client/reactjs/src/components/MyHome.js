0370554169d86df54724522f3e311580586b0bde
Jeff <jeffscottlord@gmail.com>
2019-09-17 17:00:59 -0400

    changes to check table

--- client/reactjs/src/components/MyHome.js
+++ client/reactjs/src/components/MyHome.js
@@ -1,11 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Typography } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
-import AppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
-import IconButton from "@material-ui/core/IconButton";
-import MenuIcon from "@material-ui/icons/Menu";
-import MaterialTable from "material-table";
+import MyCheckTable from "../components/MyCheckTable";
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -21,29 +17,22 @@ const useStyles = makeStyles(theme => ({
 
 export default function MyHome(props) {
   const classes = useStyles();
-  const [state, setState] = useState({
+  const [testState, setTestState] = useState({
+    cvName: "Test Calc View",
+    cvVersion: "3.0",
     columns: [
       { title: "Check", field: "check" },
       {
         title: "Found",
         field: "found",
         cellStyle: rowData => ({
-          // backgroundColor: {
-          // console.log(rowData);
-          // if (rowData === "Yes") {
-          //   backgroundColor= "#FFF";
-          // }
           backgroundColor: rowData === "Yes" ? "#e57373" : "#a5d6a7"
-          // })
-          // console.log(rowData);
         })
-      },
-      { title: "Fix", field: "fix" }
+      }
     ],
     data: [
-      { check: "Split Nodes", found: "Yes", fix: "Test3" },
-      { check: "Right Joins", found: "No", fix: "Test3" },
-      { check: "Other", found: "Yes", fix: "Test5" }
+      { check: "Split Nodes", found: "Yes" },
+      { check: "Right Joins", found: "No" }
     ],
     options: {
       selection: true,
@@ -78,13 +67,7 @@ export default function MyHome(props) {
         limited selection of things.
       </Typography>
       <div style={{ maxWidth: "50%" }}>
-        <MaterialTable
-          title="Check Fix"
-          columns={state.columns}
-          data={state.data}
-          options={state.options}
-          actions={state.actions}
-        ></MaterialTable>
+        <MyCheckTable tableState={testState}></MyCheckTable>
       </div>
     </div>
   );
